db1667703021b858006276444148071e658a038b
app_e8d01e6b <app_e8d01e6b@ip-10-169-1-220.ec2.internal>
2014-04-25 01:10:19 +0000

    AndroidKickstartR generation

---
+++ FindTune/src/com/kepler22media/findtune/FindTuneConsoleActivity.java
@@ -0,0 +1,25 @@
+
+package com.kepler22media.findtune;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+import com.googlecode.androidannotations.annotations.AfterViews;
+import com.googlecode.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class FindTuneConsoleActivity
+    extends SherlockFragmentActivity
+{
+
+
+    @AfterViews
+    void afterViews() {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getSupportMenuInflater();
+        return true;
+    }
+
+}
